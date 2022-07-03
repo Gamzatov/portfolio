da8dda0ffb9367a5d6155f816d8c09876d7a77f6
Gamzatov <dirksenartem@gmail.com>
2022-07-03 19:31:52 +0300

    styles

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -49,24 +49,24 @@ const Works = () => {
                         </div>
                     </div>
                 </div>
-                <div className={s.item}>
-                    <div className={s.item_img}>
-                        <img src={testway} alt="Pizza" />
-                    </div>
-                    <div className={s.item_text}>
-                        <p className={s.title}>
-                            TestWay
-                        </p>
-                        <p className={s.description}>
-                            Simple page concentrate on register users for some database.
-                        </p>
-                        <div className={s.link}>
-                            <a href="https://gamzatov.github.io/testWay/">
-                                Visit
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {/*<div className={s.item}>*/}
+                {/*    <div className={s.item_img}>*/}
+                {/*        <img src={testway} alt="Pizza" />*/}
+                {/*    </div>*/}
+                {/*    <div className={s.item_text}>*/}
+                {/*        <p className={s.title}>*/}
+                {/*            TestWay*/}
+                {/*        </p>*/}
+                {/*        <p className={s.description}>*/}
+                {/*            Simple page concentrate on register users for some database.*/}
+                {/*        </p>*/}
+                {/*        <div className={s.link}>*/}
+                {/*            <a href="https://gamzatov.github.io/testWay/">*/}
+                {/*                Visit*/}
+                {/*            </a>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
             </div>
         </motion.div>
