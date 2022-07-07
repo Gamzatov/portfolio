c5d4c2d351f67aca2b5bd78e19444cce166f538b
Gamzatov <dirksenartem@gmail.com>
2022-07-07 14:23:41 +0300

    sidebar fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,11 +5,13 @@ import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
     const [menu, setMenu] = React.useState(false);
     const handleMenu = () => {
-        // if (screen.width == 640) {
-        //     setMenu(!menu)
-        // }
-
+        if (window.screen.width <= 1023) {
+            setMenu(!menu);
+        }
+        
     }
+    let wind = window.screen.width
+    console.log(wind)
     return (
         <div className="side_wrapper">
             <div className="mob_btn">
@@ -22,26 +24,26 @@ const Sidebar = () => {
 
                 <ul className={s.links_list_wrapper}>
                     <li className={s.sidebar_link}>
-                        <NavLink 
+                        <NavLink onClick={()=> handleMenu()}
                             className={({ isActive }) => (isActive ? 'is-active' : 'inactive')}
                             to='/portfolio/'><span className={s.icon_holder}><ion-icon className={s.icon}
                                 name="home-outline"></ion-icon></span> Home
                         </NavLink>
                     </li>
                     <li className={s.sidebar_link}>
-                        <NavLink 
+                        <NavLink onClick={()=> handleMenu()}
                             className={({ isActive }) => (isActive ? 'is-active' : 'inactive')} to='portfolio/about'>
                             <span className={s.icon_holder}><ion-icon className={s.icon}
                                 name="person-outline"></ion-icon> </span>About</NavLink>
                     </li>
                     <li className={s.sidebar_link}>
-                        <NavLink 
+                        <NavLink onClick={()=> handleMenu()}
                             className={({ isActive }) => (isActive ? 'is-active' : 'inactive')}
                             to='portfolio/experience'> <span className={s.icon_holder}><ion-icon
                                 name="school-outline"></ion-icon> </span> Experience</NavLink>
                     </li>
                     <li className={s.sidebar_link}>
-                        <NavLink 
+                        <NavLink onClick={()=> handleMenu()}
                             className={({ isActive }) => (isActive ? 'is-active' : 'inactive')} to='portfolio/works'>
                             <span className={s.icon_holder}><ion-icon className={s.icon}
                                 name="construct-outline"></ion-icon> </span>Works</NavLink>
